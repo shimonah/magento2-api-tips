54512a3dbf9277975f687dbcab65001259d47097
shimonah <provodnik357@gmail.com>
2018-05-24 12:04:14 +0300

    Create rest-phpcode-request.php

---
+++ token-based/rest-phpcode-request.php
@@ -0,0 +1,19 @@
+$userData = array("username" => "alexey", "password" => "ghjdjlybr+7");
+var_dump(json_encode($userData));
+die('stop');
+$ch = curl_init("http://magento2.loc/index.php/rest/V1/integration/admin/token");
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($userData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Content-Lenght: " . strlen(json_encode($userData))));
+
+$token = curl_exec($ch);
+
+$ch = curl_init("http://magento2.loc/index.php/rest/V1/products/24-WB04");
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Authorization: Bearer " . json_decode($token)));
+
+$result = curl_exec($ch);
+
+var_dump($result);
